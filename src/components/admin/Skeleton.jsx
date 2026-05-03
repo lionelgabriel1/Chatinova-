3bb0dc1ce7fb4b70d8ac0780d8c807d3f3fa9d43
Lionel Gabriel <gabriellionel528@gmail.com>
2026-05-03 17:36:44 +0100

    chore: limpeza completa do repositório e remoção de lixo

--- src/components/admin/Skeleton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Skeleton({ className }) {
-  return (
-    <div className={`animate-pulse bg-slate-800/50 rounded-lg ${className}`}></div>
-  );
-}
