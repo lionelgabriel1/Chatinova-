a7612f8de130280438a443a3c396db8298d7b70f
Leonel <gabriellionel528@gmail.com>
2026-05-05 09:50:37 +0000

    Corrigir outDir do Vite para dist

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/var/www/inovachat',
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
